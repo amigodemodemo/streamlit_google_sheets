0ad79b8e38ad73a880b5941b8b4d2ec832c711cf
amigodemodemo <157417731+amigodemodemo@users.noreply.github.com>
2024-01-22 22:46:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,3 @@
-# streamlit_app.py
-
 import streamlit as st
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
@@ -11,21 +9,14 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
-
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-
 df = conn.read()
-
-
 
 st.title("Simple Google Sheets example using Streamlit")
 
 """
-
 Hi there! This is a simple example how to embed an interactive table on a medium blog!
-
-
 """
 
 # ---- SIDEBAR ----
@@ -33,5 +24,15 @@ st.sidebar.header("Settings and Filters")
 
 st.write(pd.DataFrame(df))
 
-# ---- SIDEBAR ----
-st.sidebar.header("Settings and Filters")
+# ---- FOOTER ----
+
+footer = """
+
+---
+
+Rerun the app to fetch the latest data. Stock prices are updated every 5 minutes - 3 hours. Fundamentals are updated every 24 hours. 
+Filters are in the sidebar :bar_chart:. Read my finance blogs here: [Medium](https://medium.com/@steffenjanbrouwer). 
+
+Information is provided 'as is' and solely for informational purposes, not for trading purposes or advice."""
+
+st.markdown(footer)
