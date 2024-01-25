5f95277d0215cb95f1a98028fd7a2847cacacdb3
amigodemodemo <157417731+amigodemodemo@users.noreply.github.com>
2024-01-25 16:29:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,19 +7,20 @@ from streamlit_gsheets import GSheetsConnection
 conn = st.connection("gsheets", type=GSheetsConnection)
 dataframe = conn.read()
 
+# Introduction text
+"""
+Hi there! This is a simple Google Sheets example how to embed an interactive table on a Medium blog with Streamlit! The data is the historic stock price of Google
+"""
+
 # Define tabs
 tab1, tab2 = st.tabs(["Table", "Graph"])
 
 # Streamlit content
-"""
-Hi there! This is a simple Google Sheets example how to embed an interactive table on a Medium blog with Streamlit! The data is the historic stock price of Google
-"""
 with tab1:
   st.write(pd.DataFrame(dataframe))
 with tab2:
   st.line_chart(pd.DataFrame(dataframe),x="Date", y="Close")
+
 """
 You can read the blog here:
 """
-
-
